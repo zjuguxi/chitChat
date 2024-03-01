a86fe2991f40d2b2addc9b745d3647df83b830cf
Zhikang <waoaoy@sina.com>
2024-02-29 19:54:33 -0800

    add demo db

---
+++ app/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS node;
+CREATE TABLE node (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    is_active INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS message;
+CREATE TABLE message (
+    id INTEGER PRIMARY KEY,
+    sender TEXT NOT NULL,
+    receiver TEXT NOT NULL
+);
