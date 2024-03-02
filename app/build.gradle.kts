d394f4bf01dc54752fba684d0e33850b898422ac
Zhikang <waoaoy@sina.com>
2024-03-01 18:01:48 -0800

    initialize sprint boot test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,10 +18,7 @@ repositories {
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation(libs.junit.jupiter)
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(libs.spring.test)
 
     // This dependency is used by the application.
     implementation(libs.guava)
