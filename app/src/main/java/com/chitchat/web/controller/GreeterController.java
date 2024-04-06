00410784e821a8e2504f4500428a3254bbd083bc
Zhikang <waoaoy@sina.com>
2024-04-06 10:35:59 -0700

    feat: add front-end

--- app/src/main/java/com/chitchat/web/controller/GreeterController.java
+++ app/src/main/java/com/chitchat/web/controller/GreeterController.java
@@ -2,32 +2,33 @@ package com.chitchat.web.controller;
 
 import com.chitchat.grpc.service.GreeterGrpc;
 import com.chitchat.grpc.service.HelloRequest;
+import com.chitchat.grpc.service.HelloResponse;
+import com.chitchat.web.dto.HelloRequestDto;
+import com.chitchat.web.dto.HelloResponseDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
-@RequestMapping(path = "/greeter")
+@RequestMapping(path = "/api/greeter")
 @Tag(name = "Greeter", description = "Greeter APIs")
 public class GreeterController {
 
     @GrpcClient("local")
     private GreeterGrpc.GreeterBlockingStub greeterStub;
 
-    @GetMapping(path = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, Object> hello(@RequestParam String name) {
+    @PostMapping(path = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public HelloResponseDto hello(@RequestBody HelloRequestDto requestDto) {
         HelloRequest request = HelloRequest.newBuilder()
-                .setName(name)
+                .setName(requestDto.getName())
                 .build();
-        Map<String, Object> map = new HashMap<>();
-        map.put("message", greeterStub.sayHello(request).getMessage());
-        return map;
+        HelloResponse response = greeterStub.sayHello(request);
+        return new HelloResponseDto(response.getMessage());
     }
 }
