a7da4587229d8c56b3c34bd26bc7973f9e38b9b6
Zhikang <waoaoy@sina.com>
2024-04-10 22:55:59 -0700

    feat: delete demo

--- app/src/main/java/com/chitchat/persistence/repository/NodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chitchat.persistence.repository;
-
-import com.chitchat.persistence.entity.Node;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NodeRepository extends CrudRepository<Node, Long> {
-}
