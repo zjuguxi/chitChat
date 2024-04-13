7ea8cc13cb05ff43291ad24a2c4f77967301c709
Zhikang <waoaoy@sina.com>
2024-04-13 09:15:01 -0700

    feat: delete demo

--- app/src/main/resources/static/script.js
+++ app/src/main/resources/static/script.js
@@ -10,25 +10,3 @@ $(document).ready(function () {
         }
     });
 });
-
-$(document).ready(function() {
-    $('#submitButton').click(function() {
-        var userName = $('#nameInput').val();
-        if (userName) {
-            $.ajax({
-                url: '/api/greeter/hello',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({name: userName}),
-                success: function(response) {
-                    $('#messageDisplay').text(response.message);
-                },
-                error: function(xhr, status, error) {
-                    $('#messageDisplay').text("Error: " + error);
-                }
-            });
-        } else {
-            $('#messageDisplay').text("Please input your name.");
-        }
-    });
-});
